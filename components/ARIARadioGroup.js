99aa910b9dfb2b1beac3cefb11882538820f3afe
aristov <vv.aristov@gmail.com>
2015-11-06 10:31:21 +0300

    radiogroup: disabled state

--- components/ARIARadioGroup.js
+++ components/ARIARadioGroup.js
@@ -3,7 +3,7 @@ function ARIARadioGroup(element) {
     element.radioGroup = this;
 }
 
-ARIARadioGroup.prototype.getGroup = function(check) {
+ARIARadioGroup.prototype.getGroup = function() {
     return this.group || (this.group = this.element.querySelectorAll('button[role=radio]'));
 }
 
@@ -37,15 +37,15 @@ ARIARadioGroup.prototype.updateValue = function(value) {
     input? input.value = value : (this.dataset.value = value);
 }
 
-ARIARadioGroup.prototype.nextOf = function(element) {
+ARIARadioGroup.prototype.nextOf = function(button) {
     var group = this.getGroup(),
-        index = this.indexOf(element) + 1;
+        index = this.indexOf(button) + 1;
     return index >= group.length? group[0] : group[index];
 }
 
-ARIARadioGroup.prototype.previousOf = function(element) {
+ARIARadioGroup.prototype.previousOf = function(button) {
     var group = this.getGroup(),
-        index = this.indexOf(element) - 1;
+        index = this.indexOf(button) - 1;
     return index < 0? group[group.length - 1] : group[index];
 }
 
@@ -79,7 +79,7 @@ ARIARadioGroup.onButtonKeydown = function(e, button) {
             target = e.keyCode >= 39?
                 radioGroup.nextOf(button) :
                 radioGroup.previousOf(button);
-        e.ctrlKey? target.focus() : radioGroup.switch(target);
+        if(!target.disabled) e.ctrlKey? target.focus() : radioGroup.switch(target);
     }
 }
 
@@ -87,10 +87,14 @@ ARIARadioGroup.isRadioButton = function(target) {
     return !!target.radioGroup || target.getAttribute('role') === 'radio';
 }
 
-document.addEventListener('click', function(e) {
-    if(ARIARadioGroup.isRadioButton(e.target)) ARIARadioGroup.onButtonClick(e, e.target);
-});
+ARIARadioGroup.attachToDocument = function(root) {
+    var _this = this;
+    root.addEventListener('click', function(e) {
+        if(_this.isRadioButton(e.target)) _this.onButtonClick(e, e.target);
+    });
+    root.addEventListener('keydown', function(e) {
+        if(_this.isRadioButton(e.target)) _this.onButtonKeydown(e, e.target);
+    });
+}
 
-document.addEventListener('keydown', function(e) {
-    if(ARIARadioGroup.isRadioButton(e.target)) ARIARadioGroup.onButtonKeydown(e, e.target);
-});
+ARIARadioGroup.attachToDocument(window.document);

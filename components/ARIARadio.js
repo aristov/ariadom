02f99ee33901cf3f3320dc01aa19ca0a949579a7
aristov <vv.aristov@gmail.com>
2015-11-12 13:22:35 +0300

    radio: handle disabled state (wip)

--- components/ARIARadio.js
+++ components/ARIARadio.js
@@ -1,8 +1,10 @@
 function ARIARadioGroup(element) {
     element.ariaradiogroup = this;
+
     this.element = element;
     this.checked = null;
     this.input = element.querySelector('input');
+
     this.initialize();
 }
 
@@ -10,46 +12,63 @@ ARIARadioGroup.prototype.forEach = Array.prototype.forEach;
 ARIARadioGroup.prototype.push = Array.prototype.push;
 
 ARIARadioGroup.prototype.initialize = function() {
-    var first, checked;
+    var first, checked, focusable;
+
     this.forEach.call(
         this.element.querySelectorAll('[role=radio]'),
         function(element, i) {
             var radio = ARIARadio.getRadio(element);
+
             if(radio.isChecked()) this.checked = checked = radio;
-            this.push(i? ((radio.prev = this[i - 1]).next = radio) : (first = radio));
+
+            if(!focusable && !radio.isDisabled()) focusable = radio;
+
+            this.push(i?
+                ((radio.prev = this[i - 1]).next = radio) :
+                (first = radio));
         }, this);
+
     (first.prev = this[this.length - 1]).next = first;
+
     if(!checked || checked.isDisabled()) {
-        var focusable = first;
-        do {
-            if(!focusable.isDisabled()) {
-                focusable.setFocusable(true);
-                break;
-            }
-        } while((focusable = focusable.next) !== first);
+        focusable && focusable.setFocusable(true);
     }
+}
+
+ARIARadioGroup.prototype.setDisabled = function(value) {
+    value?
+        this.element.setAttribute('aria-disabled', 'true') :
+        this.element.removeAttribute('aria-disabled');
 }
 
 ARIARadioGroup.prototype.setChecked = function(radio) {
     var checked = this.checked;
-    checked? checked.setChecked('false') : this[0].setFocusable(false);
+
+    checked?
+        checked.setChecked('false') :
+        this.forEach(function(radio) { // may be optimized
+            radio.setFocusable(false);
+        });
+
     (this.checked = radio).setChecked('true');
     this.input && (this.input.value = radio.getValue());
 }
 
 ARIARadioGroup.prototype.focus = function() {
-    (this.checked || this[0]).element.focus();
+    var focusable = this.element.querySelector('[tabindex="0"]');
+    focusable && focusable.focus();
 }
 
 ARIARadioGroup.prototype.getValue = function() {
     return this.input?
         this.input.value :
-        this.checked? this.checked.getValue() : '';
+        (this.checked? this.checked.getValue() : '');
 }
 
 ARIARadioGroup.prototype.setValue = function(value) {
     var first = this[0],
         radio = first;
+
     do {
         if(radio.getValue() === value) {
             this.setChecked(radio);
@@ -66,17 +85,23 @@ ARIARadioGroup.getGroup = function(element) {
 
 function ARIARadio(element) {
     element.ariaradio = this;
+
     this.element = element;
     this.setFocusable(this.isChecked() && !this.isDisabled());
     this.group = this.getGroup();
+
     element.addEventListener('keydown', this.onKeydown.bind(this));
 }
 
 ARIARadio.prototype.onKeydown = function(e) {
     var keyCode = e.keyCode;
+
     if(keyCode >= 37 && keyCode <=40) {
         var direction = keyCode < 39? 'prev' : 'next',
             target = this[direction];
+
+        e.preventDefault();
+
         do {
             if(!target.isDisabled()) {
                 this.group.setChecked(target);
@@ -108,6 +133,7 @@ ARIARadio.prototype.setFocusable = function(value) {
 
 ARIARadio.prototype.setChecked = function(value) {
     var element = this.element;
+
     if(value === 'true') {
         this.setFocusable(true);
         element.setAttribute('aria-checked', 'true');
@@ -144,6 +170,7 @@ ARIARadio.onClick = function(e, element) {
 
 ARIARadio.attachToDocument = function() {
     var _this = this;
+
     document.addEventListener('focus', function(e) {
         if(_this.isRadio(e.target)) _this.onFocus(e, e.target);
     }, true);

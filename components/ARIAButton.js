51cdaf63afc00e3af4f1e0a7992b9992587a16ec
aristov <vv.aristov@gmail.com>
2015-11-29 04:48:49 +0300

    ARIAButton interface

--- components/ARIAButton.js
+++ components/ARIAButton.js
@@ -1,22 +1,39 @@
 function ARIAButton(element) {
-    if(element.role) return element;
-
-    Object.defineProperty(element, 'role', {
-        enumerable : true,
-        get : function() {
-            return this.getAttribute('role') || '';
-        }
-    });
-
-    Object.defineProperty(element, 'ariaPressed', {
-        enumerable : true,
-        get : function() {
-            return this.getAttribute('aria-pressed') || '';
-        },
-        set : function(value) {
-            this.setAttribute('aria-pressed', String(value));
-        }
-    });
-
-    return element;
+    element.aria = this;
+    this.element = element;
 }
+
+Object.defineProperty(ARIAButton.prototype, 'pressed', {
+    enumerable : true,
+    get : function() {
+        return this.element.getAttribute('aria-pressed') || '';
+    },
+    set : function(value) {
+        this.element.setAttribute('aria-pressed', String(value));
+    }
+});
+
+ARIAButton.prototype.onClick = function(e) {
+    if(this.pressed) {
+        this.pressed = this.pressed === 'true'? 'false' : 'true';
+        this.element.dispatchEvent(new Event('change'));
+    };
+}
+
+ARIAButton.isButton = function(element) {
+    return Boolean(element.aria) || element.tagName === 'BUTTON';
+}
+
+ARIAButton.getButton = function(element) {
+    return element.aria || new ARIAButton(element);
+}
+
+ARIAButton.attachToDocument = function() {
+    var _this = this;
+
+    document.addEventListener('click', function(e) {
+        if(_this.isButton(e.target)) _this.getButton(e.target).onClick(e);
+    }, true);
+}
+
+ARIAButton.attachToDocument();

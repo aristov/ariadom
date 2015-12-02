5c2d16aa3a0abea4cff6b926bff6fd8f470a31f7
aristov <vv.aristov@gmail.com>
2015-12-03 01:14:55 +0300

    ARIAListBox arrow keys

--- components/ARIAListBox.js
+++ components/ARIAListBox.js
@@ -2,25 +2,67 @@ function ARIAListBox(element) {
     element.aria = this;
     this.element = element;
     this.createList();
+    element.addEventListener('keydown', this.onKeyDown.bind(this));
 }
 
+ARIAListBox.prototype = new Array();
+
+Object.defineProperty(ARIAListBox.prototype, 'selected', {
+    enumerable : true,
+    get : function() {
+        return this.filter(function(option) {
+            return option.selected === 'true';
+        });
+    }
+});
+
 ARIAListBox.prototype.createList = function() {
-    Array.prototype.forEach.call(
+    this.forEach.call(
         this.element.querySelectorAll('[role=option]'),
         function(element, i) {
-            Array.prototype.push.call(this, ARIAOption.getOption(element));
+            this.push(ARIAOption.getOption(element));
         }, this);
 }
 
 ARIAListBox.prototype.unselect = function() {
-    Array.prototype.forEach.call(this, function(option) {
+    this.forEach(function(option) {
         option.selected = 'false';
     });
+}
+
+ARIAListBox.prototype.onKeyDown = function(e) {
+    var keyCode = e.keyCode;
+
+    if(keyCode >= 37 && keyCode <= 40) {
+        e.preventDefault(); // prevent page scrolling
+        var direction = keyCode < 39? -1 : 1,
+            selected = this.selected[0],
+            next = this.indexOf(selected) + direction;
+        if(next === this.length) next = 0;
+        if(next < 0) next = this.length - 1;
+        this.unselect();
+        this[next].selected = true;
+    }
+}
+
+ARIAListBox.isListBox = function(element) {
+    return Boolean(element.aria) ||
+        (typeof element.getAttribute === 'function' &&
+            element.getAttribute('role') === 'listbox');
 }
 
 ARIAListBox.getListBox = function(element) {
     return element.aria || new ARIAListBox(element);
 }
+
+ARIAListBox.attachToDocument = function() {
+    var _this = this;
+    document.addEventListener('focus', function(e) {
+        if(_this.isListBox(e.target)) _this.getListBox(e.target);
+    }, true);
+}
+
+ARIAListBox.attachToDocument();
 
 ////////////////////////////////////////////////////////////////
 
@@ -62,7 +104,6 @@ ARIAOption.getOption = function(element) {
 
 ARIAOption.attachToDocument = function() {
     var _this = this;
-
     document.addEventListener('mousedown', function(e) {
         if(_this.isOption(e.target)) _this.getOption(e.target).onMouseDown(e);
     }, true);

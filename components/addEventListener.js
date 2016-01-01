7861eac5065ff25545539c91c9d77960d5810d41
aristov <vv.aristov@gmail.com>
2016-01-01 12:57:19 +0300

    remove redundant code

--- components/addEventListener.js
+++
@@ -1,9 +0,0 @@
-if(!Node.prototype.addEventListener && Node.prototype.attachEvent) {
-    Node.prototype.addEventListener = function(eType, cb) {
-        this.attachEvent('on' + eType, function(e) {
-            e.currentTarget = this;
-            e.target = e.srcElement;
-            cb.apply(this, arguments);
-        })
-    };
-}

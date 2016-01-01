cb4c50b6ad5eeddd86806af24beaf92f9933a0d4
aristov <vv.aristov@gmail.com>
2016-01-01 12:52:35 +0300

    textarea support

--- design/ARIAHTMLTextBox.js
+++ design/ARIAHTMLTextBox.js
@@ -2,7 +2,7 @@ function ARIAHTMLTextBox(element) {
     element.aria = this;
     this.element = element;
 
-    (this.input = element.querySelector('input'))
+    (this.input = element.querySelector('input,textarea'))
         .addEventListener('blur', this.onBlur.bind(this));
 }
 
@@ -17,6 +17,16 @@ Object.defineProperty(ARIAHTMLTextBox.prototype, 'disabled', {
         (this.input.disabled = disabled === 'true')?
             this.element.classList.add('disabled') :
             this.element.classList.remove('disabled');
+    }
+});
+
+Object.defineProperty(ARIAHTMLTextBox.prototype, 'value', {
+    enumerable : true,
+    get : function() {
+        return this.input.value;
+    },
+    set : function(value) {
+        this.input.value = value;
     }
 });
 
@@ -38,8 +48,9 @@ ARIAHTMLTextBox.getTextBox = function(element) {
 
 ARIAHTMLTextBox.attachToDocument = function() {
     document.addEventListener('focus', function(event) {
-        var target = event.target;
-        if(target.tagName === 'INPUT') {
+        var target = event.target,
+            tagName = target.tagName;
+        if(tagName === 'INPUT' || tagName === 'TEXTAREA') {
             var element = target.closest('[role=textbox]'),
                 textBox = element && this.getTextBox(element);
             if(textBox) textBox.onFocus(event);
